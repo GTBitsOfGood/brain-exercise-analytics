10487b0c0506ad41adea44b6026cd5ee1c8e09b3
Johannes Qian <115521998+johannesq23@users.noreply.github.com>
2023-09-19 22:08:08 -0400

    Add all routes and methods for analytics endpoint (#3)
    * Add all routes and methods for analytics endpoint
    
    * Run yarn lint
    
    * Fix eslint formatting
    
    * Fix Prettier
    
    Sorry taking so many commits, first time linting
    
    * Run yarn format
    
    * Fixed .eslintrc rules
    
    * fix requested changes

--- common_utils/types.ts
+++ common_utils/types.ts
@@ -3,6 +3,16 @@ export enum Role {
   NONPROFIT_USER = "Nonprofit User",
 }
 
+export enum Days {
+  Sunday,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+}
+
 export interface IUser {
   // the unqiue id assigned to a user. Let MongoDB create this when you insert a document
   // without any_id attribute
@@ -26,4 +36,15 @@ export interface SignupData {
   birthDate: Date;
   secondaryContactName: string;
   secondaryContactPhone: string;
+}
+
+export interface IAnalytics {
+  // the unqiue id assigned to a user. Let MongoDB create this when you insert a document
+  // without any_id attribute
+  _id?: string;
+  userID: string;
+  totalSessionsCompleted: number;
+  streak: [string];
+  lastSessionMetrics: object;
+  weeklyMetrics: [object];
 }
