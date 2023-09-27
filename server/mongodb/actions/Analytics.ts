b73d30a18132984afb604d64fa3a62a4ebd53b1a
johannesq23 <johannesq23@gmail.com>
2023-09-27 00:54:04 -0400

    add average-weekly-stats and refactored names

--- server/mongodb/actions/Analytics.ts
+++ server/mongodb/actions/Analytics.ts
@@ -1,5 +1,6 @@
 import Analytics from "@server/mongodb/models/Analytics";
 import { Days, IAnalytics } from "@/common_utils/types";
+import { getCurrentMonday } from "@server/utils/utils";
 
 export const getAnalyticsByID = async (
   userID: string,
@@ -122,4 +123,257 @@ export const updateSessionComplete = async (
   );
 
   return data;
+};
+
+// average weekly stats function
+export const averageWeeklyStats = async () => {
+  // divide all values by completed sessions and added streaklength
+  await Analytics.updateMany({ _id: "6500ab5fb837932f99797538" }, [
+    {
+      $set: {
+        weeklyMetrics: {
+          $map: {
+            input: {
+              $range: [0, { $size: "$weeklyMetrics" }],
+            },
+            in: {
+              $cond: [
+                { $eq: ["$$this", 0] },
+                {
+                  $mergeObjects: [
+                    { $arrayElemAt: ["$weeklyMetrics", "$$this"] },
+                    {
+                      streakLength: { $size: "$streak" },
+                      math: {
+                        $let: {
+                          vars: {
+                            firstItem: {
+                              $arrayElemAt: ["$weeklyMetrics", 0],
+                            },
+                          },
+                          in: {
+                            sessionsCompleted:
+                              "$$firstItem.math.sessionsCompleted",
+                            questionsAttempted: {
+                              $cond: [
+                                {
+                                  $eq: [
+                                    "$$firstItem.math.sessionsCompleted",
+                                    0,
+                                  ],
+                                },
+                                0,
+                                {
+                                  $divide: [
+                                    "$$firstItem.math.questionsAttempted",
+                                    "$$firstItem.math.sessionsCompleted",
+                                  ],
+                                },
+                              ],
+                            },
+                            questionsCorrect: {
+                              $cond: [
+                                {
+                                  $eq: [
+                                    "$$firstItem.math.sessionsCompleted",
+                                    0,
+                                  ],
+                                },
+                                0,
+                                {
+                                  $divide: [
+                                    "$$firstItem.math.questionsCorrect",
+                                    "$$firstItem.math.sessionsCompleted",
+                                  ],
+                                },
+                              ],
+                            },
+                            finalDifficultyScore: {
+                              $cond: [
+                                {
+                                  $eq: [
+                                    "$$firstItem.math.sessionsCompleted",
+                                    0,
+                                  ],
+                                },
+                                0,
+                                {
+                                  $divide: [
+                                    "$$firstItem.math.finalDifficultyScore",
+                                    "$$firstItem.math.sessionsCompleted",
+                                  ],
+                                },
+                              ],
+                            },
+                            timePerQuestion: {
+                              $cond: [
+                                {
+                                  $eq: [
+                                    "$$firstItem.math.sessionsCompleted",
+                                    0,
+                                  ],
+                                },
+                                0,
+                                {
+                                  $divide: [
+                                    "$$firstItem.math.timePerQuestion",
+                                    "$$firstItem.math.sessionsCompleted",
+                                  ],
+                                },
+                              ],
+                            },
+                          },
+                        },
+                      },
+                      trivia: {
+                        $let: {
+                          vars: {
+                            firstItem: {
+                              $arrayElemAt: ["$weeklyMetrics", 0],
+                            },
+                          },
+                          in: {
+                            sessionsCompleted:
+                              "$$firstItem.trivia.sessionsCompleted",
+                            questionsAttempted: {
+                              $cond: [
+                                {
+                                  $eq: [
+                                    "$$firstItem.trivia.sessionsCompleted",
+                                    0,
+                                  ],
+                                },
+                                0,
+                                {
+                                  $divide: [
+                                    "$$firstItem.trivia.questionsAttempted",
+                                    "$$firstItem.trivia.sessionsCompleted",
+                                  ],
+                                },
+                              ],
+                            },
+                            questionsCorrect: {
+                              $cond: [
+                                {
+                                  $eq: [
+                                    "$$firstItem.trivia.sessionsCompleted",
+                                    0,
+                                  ],
+                                },
+                                0,
+                                {
+                                  $divide: [
+                                    "$$firstItem.trivia.questionsCorrect",
+                                    "$$firstItem.trivia.sessionsCompleted",
+                                  ],
+                                },
+                              ],
+                            },
+                            timePerQuestion: {
+                              $cond: [
+                                {
+                                  $eq: [
+                                    "$$firstItem.trivia.sessionsCompleted",
+                                    0,
+                                  ],
+                                },
+                                0,
+                                {
+                                  $divide: [
+                                    "$$firstItem.trivia.timePerQuestion",
+                                    "$$firstItem.trivia.sessionsCompleted",
+                                  ],
+                                },
+                              ],
+                            },
+                          },
+                        },
+                      },
+                      reading: {
+                        $let: {
+                          vars: {
+                            firstItem: {
+                              $arrayElemAt: ["$weeklyMetrics", 0],
+                            },
+                          },
+                          in: {
+                            sessionsAttempted:
+                              "$$firstItem.reading.sessionsAttempted",
+                            sessionsCompleted:
+                              "$$firstItem.reading.sessionsCompleted",
+                            passagesRead: {
+                              $cond: [
+                                {
+                                  $eq: [
+                                    "$$firstItem.reading.sessionsAttempted",
+                                    0,
+                                  ],
+                                },
+                                0,
+                                {
+                                  $divide: [
+                                    "$$firstItem.reading.passagesRead",
+                                    "$$firstItem.reading.sessionsAttempted",
+                                  ],
+                                },
+                              ],
+                            },
+                          },
+                        },
+                      },
+                    },
+                  ],
+                },
+                {
+                  $arrayElemAt: ["$weeklyMetrics", "$$this"],
+                },
+              ],
+            },
+          },
+        },
+      },
+    },
+  ]);
+
+  // set streak to empty array and push new empty array
+  await Analytics.updateMany(
+    { _id: "6500ab5fb837932f99797538" },
+    {
+      $push: {
+        weeklyMetrics: {
+          $each: [
+            {
+              date: getCurrentMonday(),
+              sessionsCompleted: 0,
+              streakLength: 0,
+              math: {
+                sessionsCompleted: 0,
+                questionsAttempted: 0,
+                questionsCorrect: 0,
+                finalDifficultyScore: 0,
+                timePerQuestion: 0,
+              },
+              trivia: {
+                sessionsCompleted: 0,
+                questionsAttempted: 0,
+                questionsCorrect: 0,
+                timePerQuestion: 0,
+              },
+              reading: {
+                sessionsAttempted: 0,
+                sessionsCompleted: 0,
+                passagesRead: 0,
+              },
+            },
+          ],
+          $position: 0,
+        },
+      },
+      $set: {
+        streak: [],
+      },
+    },
+  );
+
+  return null;
 };
