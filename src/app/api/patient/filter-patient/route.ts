b6b50b995f6d6bfeec95d64a584f463d9340b4a3
johannesq23 <johannesq23@gmail.com>
2023-10-31 23:15:29 -0400

    Update filtering to take in arrays

--- src/app/api/patient/filter-patient/route.ts
+++ src/app/api/patient/filter-patient/route.ts
@@ -3,18 +3,32 @@ import { getUsersFiltered } from "@server/mongodb/actions/User";
 
 export const dynamic = "force-dynamic";
 
-export const GET = APIWrapper({
+type RequestData = {
+  paramsObject: {
+    name?: string;
+    dateOfBirth?: string[];
+    email?: string[];
+    additionalAffiliation?: string[];
+    secondName?: string[];
+    secondaryPhone?: string[];
+    beiChapter?: string[];
+    active?: boolean;
+    country?: string[];
+    state?: string[];
+    city?: string[];
+    dateOfJoin?: string[];
+  };
+  page: number;
+};
+
+export const POST = APIWrapper({
   // config: {
   //   requireToken: true,
   // },
   handler: async (req) => {
-    const { searchParams } = new URL(req.url);
+    const reqdata: RequestData = (await req.json()) as RequestData;
 
-    const pageNumber = searchParams.get("page") as unknown as number;
-    const paramsObject = Object.fromEntries(searchParams.entries());
-    delete paramsObject.page;
-
-    const users = await getUsersFiltered(paramsObject, pageNumber);
+    const users = await getUsersFiltered(reqdata.paramsObject, reqdata.page);
     return users;
   },
 });
