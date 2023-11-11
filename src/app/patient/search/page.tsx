29349bdc625b45eca266a54b9f768172c48e79b7
Samarth Chandna <samarthchandna52@gmail.com>
2023-11-11 13:33:49 -0500

    added temp styling fixes for navigation panel (needs more work)

--- src/app/patient/search/page.tsx
+++ src/app/patient/search/page.tsx
@@ -1,24 +1,14 @@
-"use client";
-
-import NavigationPanel from "@src/components/NavigationPanel/NavigationPanel";
 import Search from "@src/components/Search/Search";
 import styles from "./page.module.css";
 
 export default function Page() {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles["navigation-panel"]}>
-        <NavigationPanel />
-      </div>
-      <div className={styles["rest-of-page"]}>
-        <div className={styles["search-container"]}>
-          <p className={styles["intro-text"]}>
-            To begin viewing analytics, search for a patient here!
-          </p>
-          <div className={styles["search-wrapper"]}>
-            <Search />
-          </div>
-        </div>
+    <div className={styles["search-container"]}>
+      <p className={styles["intro-text"]}>
+        To begin viewing analytics, search for a patient here!
+      </p>
+      <div className={styles["search-wrapper"]}>
+        <Search />
       </div>
     </div>
   );
--- src/components/NavigationPanel/NavigationPanel.tsx
+++ src/components/NavigationPanel/NavigationPanel.tsx
@@ -1,16 +1,11 @@
+"use client";
+
 import React from "react";
-import { Poppins } from "next/font/google";
 import { useRouter, usePathname } from "next/navigation";
 import { faChartSimple } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Metric from "./Metric/Metric";
 import styles from "./NavigationPanel.module.css";
-
-const poppins = Poppins({
-  subsets: ["latin-ext"],
-  variable: "--font-poppins",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
 
 const NavigationPanel = () => {
   const router = useRouter();
@@ -23,82 +18,80 @@ const NavigationPanel = () => {
         src="https://c.animaapp.com/2gdwBOyI/img/bei-1-1@2x.png"
         alt="BEI Image"
       />
-      <main className={poppins.variable}>
-        <div className={styles["text-wrapper"]}>
-          <span className={styles.bei}>Brain Exercise Initiative</span>
-          <span className={styles["volunteer-portal"]}>Volunteer Portal</span>
-          <div className={styles.divider}>
-            <hr />
-          </div>
-          <div className={styles[`search-patient-container`]}>
-            <div
-              className={
-                styles[
-                  `search-patient-${
-                    currentPath.startsWith("/patient/search")
-                      ? "active"
-                      : "inactive"
-                  }`
-                ]
-              }
-              onClick={() => router.push("/patient/search")}
-            >
-              <div className={styles["icon-shadow"]}>
-                <FontAwesomeIcon
-                  className={styles["statistics-icon"]}
-                  icon={faChartSimple}
-                  size="sm"
-                />
-              </div>
-              <span className={styles["search-patient-text"]}>
-                SEARCH PATIENT
-              </span>
+      <div className={styles["text-wrapper"]}>
+        <span className={styles.bei}>Brain Exercise Initiative</span>
+        <span className={styles["volunteer-portal"]}>Volunteer Portal</span>
+        <div className={styles.divider}>
+          <hr />
+        </div>
+        <div className={styles[`search-patient-container`]}>
+          <div
+            className={
+              styles[
+                `search-patient-${
+                  currentPath.startsWith("/patient/search")
+                    ? "active"
+                    : "inactive"
+                }`
+              ]
+            }
+            onClick={() => router.push("/patient/search")}
+          >
+            <div className={styles["icon-shadow"]}>
+              <FontAwesomeIcon
+                className={styles["statistics-icon"]}
+                icon={faChartSimple}
+                size="sm"
+              />
             </div>
-          </div>
-          <div className={styles["metrics-container"]}>
-            <div
-              className={
-                styles[
-                  `overall-metrics-container-${
-                    currentPath.startsWith("/patient/dashboard")
-                      ? "active"
-                      : "inactive"
-                  }`
-                ]
-              }
-              onClick={() => router.push("/patient/dashboard")}
-            >
-              <div className={styles["icon-shadow"]}>
-                <FontAwesomeIcon
-                  className={styles["statistics-icon"]}
-                  icon={faChartSimple}
-                  size="sm"
-                />
-              </div>
-              <div className={styles["overall-metrics"]}>
-                <span>PATIENT OVERALL METRICS</span>
-              </div>
-            </div>
-            <Metric title="math" />
-            <Metric title="reading" />
-            <Metric title="writing" />
-            <Metric title="trivia" />
-          </div>
-          <div className={styles.divider}>
-            <hr />
-          </div>
-          <div className={styles["patient-container"]}>
-            <img
-              className={styles["patient-pfp"]}
-              src="https://via.placeholder.com/81x81"
-            />
-            <div className={styles["patient-info"]}>
-              <span className={styles["user-name"]}>User Name</span>
-              <span className={styles.position}>Position or title</span>
-            </div>
+            <span className={styles["search-patient-text"]}>
+              SEARCH PATIENT
+            </span>
           </div>
         </div>
-      </main>
+        <div className={styles["metrics-container"]}>
+          <div
+            className={
+              styles[
+                `overall-metrics-container-${
+                  currentPath.startsWith("/patient/dashboard")
+                    ? "active"
+                    : "inactive"
+                }`
+              ]
+            }
+            onClick={() => router.push("/patient/dashboard")}
+          >
+            <div className={styles["icon-shadow"]}>
+              <FontAwesomeIcon
+                className={styles["statistics-icon"]}
+                icon={faChartSimple}
+                size="sm"
+              />
+            </div>
+            <div className={styles["overall-metrics"]}>
+              <span>PATIENT OVERALL METRICS</span>
+            </div>
+          </div>
+          <Metric title="math" />
+          <Metric title="reading" />
+          <Metric title="writing" />
+          <Metric title="trivia" />
+        </div>
+        <div className={styles.divider}>
+          <hr />
+        </div>
+        <div className={styles["patient-container"]}>
+          <img
+            className={styles["patient-pfp"]}
+            src="https://via.placeholder.com/81x81"
+          />
+          <div className={styles["patient-info"]}>
+            <span className={styles["user-name"]}>User Name</span>
+            <span className={styles.position}>Position or title</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
