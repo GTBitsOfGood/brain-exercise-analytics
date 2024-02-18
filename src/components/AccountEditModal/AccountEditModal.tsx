80e54a986f9c25b0114976178a8d8735fe519037
caileanhtuan <connorcai180@gmail.com>
2024-02-18 15:55:03 -0500

    feat: add avatar

--- src/components/AccountEditModal/AccountEditModal.tsx
+++ src/components/AccountEditModal/AccountEditModal.tsx
@@ -4,13 +4,46 @@ const Modal = ({ closeModal }) => {
   return (
     <div className={styles.container}>
       <div className={styles.left}>
-        <div>Profile</div>
-        <div>Password</div>
-        <div>Sign Out</div>
+        <div className={styles.profile}>Profile</div>
+        <div className={styles.password}>Password</div>
+        <div className={styles.signout}>Sign Out</div>
       </div>
-      <div>
-        <span onClick={closeModal}>&times;</span>
-        <h1>Testing my modal</h1>
+      <div className={styles.vl}></div>
+      <div className={styles.info}>
+        {/* <span onClick={closeModal}>&times;</span> */}
+        <div className={styles.header}>
+          <img
+            src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/141.jpg"
+            alt="Description of the image"
+          />
+          <div className={styles.myContainer}>
+            <div className={styles.first}>
+              <div>userName</div>
+              <div>status</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.inputs}>
+          <div className={styles.inputField}>
+            <label>Name:</label>
+            <input placeholder="      First Name Last Name" name="myInput" />
+          </div>
+
+          <div className={styles.inputField}>
+            <label>Date of Birth:</label>
+            <input placeholder="      MM/DD/YY" name="myInput" />
+          </div>
+
+          <div className={styles.inputField}>
+            <label>Phone:</label>
+            <input placeholder="      (xxx) xxx-xxxx" name="myInput" />
+          </div>
+
+          <div className={styles.inputField}>
+            <label>Email:</label>
+            <input placeholder="      Email" name="myInput" />
+          </div>
+        </div>
       </div>
     </div>
   );
