28761b4a83cd5947d3d97c4cf0f87974ae56155f
tylerttam <tylertam571@gmail.com>
2024-02-19 01:15:36 -0500

    Finished VolunteerApprovalGrid - have to click button twice to delete from list

--- src/components/VolunteerApprovalGrid/VolunteerApprovalGrid.tsx
+++ src/components/VolunteerApprovalGrid/VolunteerApprovalGrid.tsx
@@ -1,129 +1,141 @@
-"use client";
+import React, { useState, useMemo } from "react";
+import DataGrid from "@src/components/DataGrid/DataGrid";
+import Pagination from "@src/components/Pagination/Pagination";
+import { SortField } from "@/common_utils/types";
+import styles from "./VolunteerApprovalGrid.module.css";
 
-import { IPatientTableEntry, SortField } from "@/common_utils/types";
-import { transformDate } from "@src/utils/utils";
-import { useMemo } from "react";
-import { GridColDef } from "@src/utils/types";
-import DataGrid from "../DataGrid/DataGrid";
-import Pagination from "../Pagination/Pagination";
-import styles from "./PatientGrid.module.css";
-import { GridRowDef } from "../PatientGrid/types";
-import { Row } from "../PatientGrid/Row/Row";
-
-interface PatientGridProps {
-  data: IPatientTableEntry[];
-  sortField: SortField | undefined;
-  setSortField: React.Dispatch<React.SetStateAction<SortField | undefined>>;
-  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
-  pageCount: number;
-  currentPage: number;
+interface IVolunteer {
+  id: number;
+  name: string;
+  title: string;
+  dateRequested: string;
 }
 
-const columns: GridColDef[] = [
-  { field: "id", headerName: "", width: 90 },
-  {
-    field: "name",
-    headerName: "First Name",
-    sortable: true,
-  },
-  {
-    field: "name",
-    headerName: "Last Name",
-    sortable: true,
-  },
-  {
-    field: "patientDetails.birthDate",
-    headerName: "Date of Birth",
-    type: "string",
-    sortable: true,
-  },
-  {
-    field: "active",
-    headerName: "Active",
-    type: "string",
-    sortable: true,
-  },
+interface GridColDef<T> {
+  field: string;
+  headerName: string;
+  sortable?: boolean;
+  renderCell?: (params: T) => JSX.Element;
+}
 
-  {
-    field: "email",
-    headerName: "Email Address",
-    type: "string",
-    sortable: true,
-  },
-
-  {
-    field: "chapter",
-    headerName: "BEI Chapter",
-    type: "string",
-    sortable: true,
-  },
-
-  {
-    field: "database",
-    headerName: "Database",
-    type: "string",
-  },
+const volunteersColumns: GridColDef<IVolunteer>[] = [
+  { field: "name", headerName: "Name", sortable: true },
+  { field: "title", headerName: "Title", sortable: true },
+  { field: "dateRequested", headerName: "Date Requested", sortable: true },
+  { field: "actions", headerName: "", sortable: false },
 ];
 
-function ColumnSizes() {
+const VolunteerGrid: React.FC<{ data: IVolunteer[] }> = ({ data }) => {
+  const [sortField, setSortField] = useState<SortField | undefined>(undefined);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const itemsPerPage = 10;
+  const pageCount = Math.ceil(data.length / itemsPerPage);
+  const [deleteVolunteerId, setDeleteVolunteerId] = useState<number | null>(
+    null,
+  );
+  const [excludedIds, setExcludedIds] = useState<number[]>([]);
+
+  const handleConfirmDelete = (id: number) => {
+    setDeleteVolunteerId(id);
+    if (deleteVolunteerId !== null) {
+      setExcludedIds((current) => [...current, deleteVolunteerId]);
+      // eslint-disable-next-line
+      data = data.filter(volunteer => !excludedIds.includes(volunteer.id) && volunteer.id !== deleteVolunteerId);
+      setDeleteVolunteerId(null);
+    }
+  };
+
+  const currentItems = useMemo(() => {
+    const start = currentPage * itemsPerPage;
+    const end = start + itemsPerPage;
+    return data
+      .filter((volunteer) => !excludedIds.includes(volunteer.id))
+      .slice(start, end);
+  }, [currentPage, itemsPerPage, data, excludedIds]);
+
+  function ColumnSizes() {
+    return (
+      <colgroup>
+        <col style={{ width: "20%" }} />
+        <col style={{ width: "20%" }} />
+        <col style={{ width: "30%" }} />
+        <col style={{ width: "20%" }} />
+      </colgroup>
+    );
+  }
+
+  // Construct Rows from the currentItems
+  const Rows = currentItems.map((volunteer) => (
+    <tr key={volunteer.id}>
+      <td>{volunteer.name}</td>
+      <td>{volunteer.title}</td>
+      <td>{volunteer.dateRequested}</td>
+      <td>
+        <div className={styles.actionsContainer}>
+          <button
+            className={styles.approveButton}
+            onClick={() => handleConfirmDelete(volunteer.id)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="19"
+              viewBox="0 0 18 19"
+              fill="none"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M9 18.5C13.9706 18.5 18 14.4706 18 9.5C18 4.52944 13.9706 0.5 9 0.5C4.02944 0.5 0 4.52944 0 9.5C0 14.4706 4.02944 18.5 9 18.5ZM13.7071 7.20711C14.0976 6.81658 14.0976 6.18342 13.7071 5.79289C13.3166 5.40237 12.6834 5.40237 12.2929 5.79289L7 11.0858L5.70711 9.79289C5.31658 9.40237 4.68342 9.40237 4.29289 9.79289C3.90237 10.1834 3.90237 10.8166 4.29289 11.2071L6.29289 13.2071C6.68342 13.5976 7.31658 13.5976 7.70711 13.2071L13.7071 7.20711Z"
+                fill="#05CD99"
+              />
+            </svg>
+            Approve
+          </button>
+          <button
+            className={styles.denyButton}
+            onClick={() => handleConfirmDelete(volunteer.id)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="19"
+              viewBox="0 0 18 19"
+              fill="none"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M9 18.5C13.9706 18.5 18 14.4706 18 9.5C18 4.52944 13.9706 0.5 9 0.5C4.02944 0.5 0 4.52944 0 9.5C0 14.4706 4.02944 18.5 9 18.5ZM6.70711 5.79289C6.31658 5.40237 5.68342 5.40237 5.29289 5.79289C4.90237 6.18342 4.90237 6.81658 5.29289 7.20711L7.58579 9.5L5.29289 11.7929C4.90237 12.1834 4.90237 12.8166 5.29289 13.2071C5.68342 13.5976 6.31658 13.5976 6.70711 13.2071L9 10.9142L11.2929 13.2071C11.6834 13.5976 12.3166 13.5976 12.7071 13.2071C13.0976 12.8166 13.0976 12.1834 12.7071 11.7929L10.4142 9.5L12.7071 7.20711C13.0976 6.81658 13.0976 6.18342 12.7071 5.79289C12.3166 5.40237 11.6834 5.40237 11.2929 5.79289L9 8.08579L6.70711 5.79289Z"
+                fill="#EA4335"
+              />
+            </svg>
+            Deny
+          </button>
+        </div>
+      </td>
+    </tr>
+  ));
+
   return (
-    <colgroup>
-      <col style={{ width: "2%" }} />
-      <col style={{ width: "10%" }} />
-      <col style={{ width: "10%" }} />
-      <col style={{ width: "11%" }} />
-      <col style={{ width: "7%" }} />
-      <col style={{ width: "18%" }} />
-      <col style={{ width: "18%" }} />
-      <col style={{ width: "8%" }} />
-    </colgroup>
-  );
-}
-
-export default function PatientGrid(params: PatientGridProps) {
-  const rows = useMemo<GridRowDef[]>(
-    () =>
-      params.data.map(
-        (v) =>
-          ({
-            id: v._id,
-            firstName: v.name.split(" ")[0],
-            lastName: v.name.split(" ")[1],
-            dateOfBirth: transformDate(v.patientDetails.birthDate),
-            active: v.active,
-            email: v.email,
-            chapter: v.chapter,
-            location: v.location,
-            dateStart: transformDate(v.startDate),
-            secondContactName: v.patientDetails.secondaryContactName,
-            secondContactPhone: v.patientDetails.secondaryContactPhone,
-            additionalAffiliation: v.patientDetails.additionalAffiliation,
-          }) as GridRowDef,
-      ),
-    [params.data],
-  );
-
-  const Rows = useMemo(
-    () => rows.map((row) => <Row key={row.id} row={row} />),
-    [rows],
-  );
-
-  return (
-    <div className={styles.Container}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        sortField={params.sortField}
-        setSortField={params.setSortField}
-        ColumnSizes={ColumnSizes}
-        Rows={Rows}
-      />
-
+    <div className={styles.volunteerGridWrapper}>
+      <div className={styles.volunteerGridTable}>
+        <DataGrid
+          columns={volunteersColumns}
+          sortField={sortField}
+          setSortField={setSortField}
+          rows={currentItems}
+          ColumnSizes={ColumnSizes}
+          Rows={Rows}
+        />
+      </div>
       <Pagination
-        setCurrentPage={params.setCurrentPage}
-        pageCount={params.pageCount}
-        currentPage={params.currentPage}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        pageCount={pageCount}
       />
     </div>
   );
-}
+};
+
+export default VolunteerGrid;
