3200ccd627760f3d9695b9c1c651ed28204e61f1
Samarth Chandna <samarthchandna52@gmail.com>
2024-02-20 02:26:21 -0500

    nits

--- src/app/api/volunteer/route.ts
+++ src/app/api/volunteer/route.ts
@@ -1,3 +1,4 @@
+import { IUser, RecursivePartial } from "@/common_utils/types";
 import { deleteFirebaseUser, updateUserEmail } from "@server/firebase/utils";
 import {
   deleteVolunteer,
@@ -13,20 +14,18 @@ export const GET = APIWrapper({
   },
   handler: async (req) => {
     const email: string | null = req.nextUrl.searchParams.get("email");
-    if (email !== null) {
-      const user = await getVolunteer(email);
-      return user;
+    if (!email) {
+      throw new Error("Email parameter is missing");
     }
 
-    return null;
+    const user = await getVolunteer(email);
+    return user;
   },
 });
 
 type PatchReq = {
   email: string;
-  newData: {
-    email?: string;
-  };
+  newData: RecursivePartial<IUser>;
 };
 
 export const PATCH = APIWrapper({
@@ -39,17 +38,17 @@ export const PATCH = APIWrapper({
     const { email }: { email: string } = reqdata;
     const { newData } = reqdata;
 
-    if (email !== null) {
-      if (newData.email !== null && email === newData.email) {
-        console.log("here");
-        await updateUserEmail(email, newData.email);
-      }
-
-      const user = await updateVolunteer(email, newData);
-      return user;
+    if (!email) {
+      throw new Error("Email parameter is missing");
     }
 
-    return null;
+    if (newData.email !== null && email === newData.email) {
+      console.log("here");
+      await updateUserEmail(email, newData.email);
+    }
+
+    const user = await updateVolunteer(email, newData);
+    return user;
   },
 });
 
@@ -65,12 +64,12 @@ export const DELETE = APIWrapper({
     const reqdata: DeleteReq = (await req.json()) as DeleteReq;
     const { email }: { email: string } = reqdata;
 
-    if (email !== null) {
-      await deleteFirebaseUser(email);
-      const user = await deleteVolunteer(email);
-      return user;
+    if (!email) {
+      throw new Error("Email parameter is missing");
     }
 
-    return null;
+    await deleteFirebaseUser(email);
+    const user = await deleteVolunteer(email);
+    return user;
   },
 });
