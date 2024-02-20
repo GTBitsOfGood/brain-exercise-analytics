e231806b90b0635736f455bcc6b437559a40eab6
BeanyBeans <143154313+BeanyBeans@users.noreply.github.com>
2024-02-20 00:27:43 -0500

    Admin Approval API Route (#81)
    * finished creating approva/denied pug content files and the admin approval api route
    
    * fixed litn issue
    
    * changed backlinkUrl for rejected status
    
    * fixed email endpoint
    
    ---------
    
    Co-authored-by: Samarth Chandna <samarthchandna52@gmail.com>

--- server/mongodb/actions/User.ts
+++ server/mongodb/actions/User.ts
@@ -1,4 +1,5 @@
 import {
+  AdminApprovalStatus,
   IPatientTableEntry,
   IUser,
   PatientSearchParams,
@@ -17,6 +18,26 @@ export const getUserByEmail = async (email: string): Promise<IUser | null> => {
 export const getUserById = async (id: string): Promise<IUser | null> => {
   const user = await User.findOne<IUser>({ _id: id });
   return user;
+};
+
+export const updateUserAdminApproval = async (
+  email: string,
+  approvalStatus: AdminApprovalStatus,
+): Promise<IUser | null> => {
+  try {
+    const updatedUser = await User.findOneAndUpdate<IUser>(
+      { email },
+      {
+        $set: {
+          approved: approvalStatus,
+        },
+      },
+    );
+
+    return updatedUser;
+  } catch (error) {
+    throw new Error("Failed to update admin approval status");
+  }
 };
 
 export const verifyUserByEmail = async (
