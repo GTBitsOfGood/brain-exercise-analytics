4997dbe98dafe0c084c708a0c76857a0c02a7f66
nikvijay07 <nikvijay07@gmail.com>
2024-09-22 22:06:20 -0400

    create Chapter model

---
+++ server/mongodb/models/Chapter.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import { AdminApprovalStatus, Role } from "@/common_utils/types";
+
+const { Schema } = mongoose;
+
+const ChapterSchema = new Schema({
+    name: {
+        type: String,
+        default: ""
+    },
+    chapterPresident: {
+        type: mongoose.Schema.Types.ObjectId,
+        require: true,
+    },
+    patients: {
+        type: Number,
+        default: 0
+    },
+    yearFounded: {
+        type: Number,
+        default: new Date().getFullYear()
+    },
+    location: {
+        type: {
+            country: String,
+            state: String,
+            city: String
+        },
+        default: {
+            country: "",
+            state: "",
+            city: "",
+          }
+    }
+
+  
+});
+
+const User = mongoose.models?.User ?? mongoose.model("Chapter", ChapterSchema);
+export default User;
