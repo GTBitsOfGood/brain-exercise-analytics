ce2980c6fe54328c682fe124ee9c36a9e4bc5e54
Johannes Qian <johannesq23@gmail.com>
2024-10-18 00:55:27 -0400

    change remove message

--- src/components/ChapterVolunteerGrid/Popup/Popup.tsx
+++ src/components/ChapterVolunteerGrid/Popup/Popup.tsx
@@ -13,8 +13,7 @@ const Popup = ({
       <div className={styles.popup}>
         <div className={styles.popupTitle}>Remove from Chapter</div>
         <div className={styles.popupContent}>
-          This will remove the volunteer from the chapter. This cannot be
-          undone.
+          This will remove the volunteer from the chapter. Please confirm to proceed.
         </div>
         <div className={styles.popupActions}>
           <button onClick={onConfirm} className={styles.confirmButton}>
