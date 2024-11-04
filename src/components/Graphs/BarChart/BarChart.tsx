ef99bb85a045b2c0b2652ef9abda25e4ebe5e402
nikvijay07 <nikvijay07@gmail.com>
2024-11-04 17:49:40 -0500

    no data found

--- src/components/Graphs/BarChart/BarChart.tsx
+++ src/components/Graphs/BarChart/BarChart.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Poppins, Inter } from "next/font/google";
-import { InfoIcon } from "@src/app/icons";
+import { ExclamationOutlinedIcon, InfoIcon } from "@src/app/icons";
 import * as d3 from "d3";
 import { MouseEvent, useCallback, useEffect, useRef, useState } from "react";
 import { D3Data } from "@src/utils/types";
@@ -221,27 +221,26 @@ export default function BarChart({
         .tickSizeInner(0)
         .tickFormat(() => "");
 
-      svg
-        .append("g")
-        .attr("class", `y-axis-vert`)
-        .attr("transform", `translate(${marginLeft - 5}, 0)`)
-        .call(axisVert);
+          svg
+            .append("g")
+            .attr("class", `y-axis-vert`)
+            .attr("transform", `translate(${marginLeft - 5}, 0)`)
+            .call(axisVert);
 
-      svg
-        .append("g")
-        .attr("class", `y-axis-grid ${styles.yAxis}`)
-        .attr("transform", `translate(${marginLeft - 5}, 0)`)
-        .call(yAxisGrid);
+          svg
+            .append("g")
+            .attr("class", `y-axis-grid ${styles.yAxis}`)
+            .attr("transform", `translate(${marginLeft - 5}, 0)`)
+            .call(yAxisGrid);
 
-      svg
-        .append("g")
-        .attr("transform", `translate(-5, ${height - marginBottom})`)
-        .attr("class", "x-axis-hor")
-        .style("font", `10px ${poppins500.style.fontFamily}`)
-        .call(axisHor);
+          svg
+            .append("g")
+            .attr("transform", `translate(-5, ${height - marginBottom})`)
+            .attr("class", "x-axis-hor")
+            .style("font", `10px ${poppins500.style.fontFamily}`)
+            .call(axisHor);
     }
-
-    svg
+      svg
       .append("g")
       .attr("transform", `translate(${barWidth / 2}, ${height - marginBottom})`)
       .attr("class", "x-axis-top")
@@ -355,6 +354,38 @@ export default function BarChart({
         );
     }
 
+
+    if (!dataExists) {
+      svg.selectAll("*").remove();
+
+
+      const iconSVG = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 40 40" fill="none">
+          <path d="M20.0021 3.33594C29.2071 3.33594 36.6688 10.7976 36.6688 20.0026C36.6688 29.2076 29.2071 36.6693 20.0021 36.6693C10.7971 36.6693 3.33545 29.2076 3.33545 20.0026C3.33545 10.7976 10.7971 3.33594 20.0021 3.33594ZM20.0021 6.66927C16.4659 6.66927 13.0745 8.07403 10.574 10.5745C8.07354 13.075 6.66878 16.4664 6.66878 20.0026C6.66878 23.5388 8.07354 26.9302 10.574 29.4307C13.0745 31.9312 16.4659 33.3359 20.0021 33.3359C23.5383 33.3359 26.9297 31.9312 29.4302 29.4307C31.9307 26.9302 33.3354 23.5388 33.3354 20.0026C33.3354 16.4664 31.9307 13.075 29.4302 10.5745C26.9297 8.07403 23.5383 6.66927 20.0021 6.66927ZM20.0021 25.0026C20.4441 25.0026 20.8681 25.1782 21.1806 25.4908C21.4932 25.8033 21.6688 26.2272 21.6688 26.6693C21.6688 27.1113 21.4932 27.5352 21.1806 27.8478C20.8681 28.1603 20.4441 28.3359 20.0021 28.3359C19.5601 28.3359 19.1362 28.1603 18.8236 27.8478C18.511 27.5352 18.3354 27.1113 18.3354 26.6693C18.3354 26.2272 18.511 25.8033 18.8236 25.4908C19.1362 25.1782 19.5601 25.0026 20.0021 25.0026ZM20.0021 10.0026C20.4441 10.0026 20.8681 10.1782 21.1806 10.4908C21.4932 10.8033 21.6688 11.2272 21.6688 11.6693V21.6693C21.6688 22.1113 21.4932 22.5352 21.1806 22.8478C20.8681 23.1603 20.4441 23.3359 20.0021 23.3359C19.5601 23.3359 19.1362 23.1603 18.8236 22.8478C18.511 22.5352 18.3354 22.1113 18.3354 21.6693V11.6693C18.3354 11.2272 18.511 10.8033 18.8236 10.4908C19.1362 10.1782 19.5601 10.0026 20.0021 10.0026Z" fill="#F30000"/>
+        </svg>
+      `;
+      const iconSize = 30;
+
+      svg
+        .append("foreignObject")
+        .attr("x", width / 2 - 97)
+        .attr("y", height / 2 - 37) // Position above the "No Data" text
+        .attr("width", iconSize)
+        .attr("height", iconSize)
+        .html(iconSVG);
+
+      svg
+        .append("text")
+        .attr("x", width / 2)
+        .attr("y", height / 2 - 20)
+        .attr("text-anchor", "middle")
+        .attr("dominant-baseline", "middle")
+        .style("font-size", "16px")
+        .style("font-family", poppins500.style.fontFamily)
+        .style("fill", "#2B3674")
+        .text("No Data Found");
+    }
+
     return () => window.removeEventListener("scroll", onScroll);
   }, [
     newData,
@@ -460,12 +491,12 @@ export default function BarChart({
           borderRadius: "12px",
         }}
       ></div>
-      <svg
-        ref={windowRef}
-        width={width}
-        height={height}
-        style={{ marginTop: 10 }}
-      ></svg>
+          <svg
+            ref={windowRef}
+            width={width}
+            height={height}
+            style={{ marginTop: 10 }}
+          ></svg>
       <div style={{ justifyContent: "center" }}>
         <div></div>
       </div>
