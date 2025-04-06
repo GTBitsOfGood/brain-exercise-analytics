ee515882cb1b42a483f74d48ca84e661793f4ba4
Nathan <nathandong68@gmail.com>
2025-04-06 17:29:58 -0400

    conditional state and city filter placeholder

--- src/components/VolunteerSearch/VolunteerAdvancedSearch/VolunteerAdvancedSearch.tsx
+++ src/components/VolunteerSearch/VolunteerAdvancedSearch/VolunteerAdvancedSearch.tsx
@@ -510,7 +510,7 @@ export const VolunteerAdvancedSearch = (props: UpdateParamProp) => {
           <SelectDropdown
             title="State"
             dropdownProps={{
-              placeholder: "Select state",
+              placeholder: countryCode == null ? "Select country first" : "Select state",
               options: STATES,
               value: state,
               onChange: (e: SelectChangeEvent<unknown>) => {
@@ -532,7 +532,7 @@ export const VolunteerAdvancedSearch = (props: UpdateParamProp) => {
           <SelectDropdown
             title="City"
             dropdownProps={{
-              placeholder: "Select city",
+              placeholder: stateCode == null ? "Select state first" : "Select city",
               options: CITIES,
               value: city,
               onChange: (e: SelectChangeEvent<unknown>) => {
